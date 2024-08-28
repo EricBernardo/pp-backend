95937fd887155879b835dba4d4e6e49b48f73598
Eric Bernardo de Sousa <eric.bernardo.sousa@gmail.com>
2024-08-28 11:10:50 -0300

    created api documentaion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,49 @@ use App\Repositories\UserRepository;
 
 class AuthController extends Controller
 {
+    /**
+     * @OA\Post (
+     *     path="/api/auth",
+     *     tags={"Generate Token"},
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                      type="object",
+     *                      @OA\Property(
+     *                          property="email",
+     *                          type="string"
+     *                      ),
+     *                      @OA\Property(
+     *                          property="password",
+     *                          type="string"
+     *                      )
+     *                 ),
+     *                 example={
+     *                     "email":"test@test.com",
+     *                     "password":"password"
+     *                }
+     *             )
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="access_token", type="string", example="1|2wcjBENbeB2esqbfEYe68vMH1Ev4HxKnkMVAaImide2cbdb3"),
+     *              @OA\Property(property="token_type", type="string", example="Bearer")
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="invalid",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Invalid credentials"),
+     *          )
+     *      )
+     * )
+     */
     public function auth(Request $request)
     {
         $validator = Validator::make($request->all(), [
